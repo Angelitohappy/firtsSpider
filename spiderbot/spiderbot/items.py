48fb16d82218ba57a9a529a017dd37cdb64009a3
Angelitohappy <127353092+Angelitohappy@users.noreply.github.com>
2024-09-15 04:03:50 -0400

    changes added

--- spiderbot/spiderbot/items.py
+++ spiderbot/spiderbot/items.py
@@ -9,4 +9,8 @@ import scrapy
 class SpiderbotItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    nameWeb = scrapy.Field()
+    url = scrapy.Field()
+    modification_date = scrapy.Field()
+    size = scrapy.Field()
+
